b85004d3a9a6692443539e625addf2cd6da525be
Megha Beria <beriamegha8@gmail.com>
2025-11-02 21:38:08 +0530

    Added Kubernetes deployment, successful local testing with kind cluster, and Trivy security scan

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ import socket
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
 
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
 
 @app.route('/status')
 def status():
